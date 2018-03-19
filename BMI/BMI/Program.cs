43dd4a9f5a2c8f6fe39befe44d3dd38faa64d408
DESKTOP-KQBVSJ0\charl <charlotte0388@gmail.com>
2018-03-19 20:28:36 +0800

    2,讓程式碼好看一點

--- BMI/BMI/Program.cs
+++ BMI/BMI/Program.cs
@@ -11,47 +11,36 @@ namespace BMI
         static void Main(string[] args)
         {
             Console.BackgroundColor = ConsoleColor.White ;
-
             Console.ForegroundColor = ConsoleColor.DarkCyan ;
+            Console.WriteLine ("＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝　　　　　　　") ;
 
-            Console.WriteLine("＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝　　　　　　　") ;
-
-            Console.WriteLine("｜　　　　　　　　　請輸入身高：　　　　　　　　　　　　　｜　　　　　　　") ;
-
+            Console.WriteLine ("｜　　　　　　　　　請輸入身高：　　　　　　　　　　　　　｜　　　　　　　") ;
             String height = Console.ReadLine() ;
-
             float H = (float) int.Parse(height) / 100 ;
 
-            Console.WriteLine("｜　　　　　　　　　請輸入體重：　　　　　　　　　　　　　｜　　　　　　　") ;
-
+            Console.WriteLine ("｜　　　　　　　　　請輸入體重：　　　　　　　　　　　　　｜　　　　　　　") ;
             String weight = Console.ReadLine() ;
 
-            float bmi =(int.Parse(weight) /(H * H));
-
-            
-
-            Console.WriteLine("｜　　　　　　　　您的BMI值是" + bmi +"　 　　　 　　　　　 ｜　　　　　　　") ;
+            float bmi =(int.Parse(weight) / (H * H));
+            Console.WriteLine ("｜　　　　　　　　您的BMI值是" + bmi +"　 　　　 　　　　　 ｜　　　　　　　") ;
 
             if(bmi<18.5)
             {
-                Console.WriteLine("｜　　　　　　　　您的BMI值過低　　　　　　　 　　　　　　｜");
+                Console.WriteLine ("｜　　　　　　　　您的BMI值過低　　　　　　　 　　　　　　｜") ;
             }
-
             if (bmi > 24)
             {
-                Console.WriteLine("｜　　　　　　　　您的BMI值過高　　　　　　　 　　　　　　｜");
+                Console.WriteLine ("｜　　　　　　　　您的BMI值過高　　　　　　　 　　　　　　｜") ;
             }
-
             if (17 > bmi || bmi > 31)
             {
-                Console.WriteLine("｜　　　　　　　　　　您免役　　　　　　　　  　　　　　　｜");
+                Console.WriteLine ("｜　　　　　　　　　　您免役　　　　　　　　  　　　　　　｜") ;
             }
             else
             {
-                Console.WriteLine("｜　　　　　　　　您的BMI值正常　　　　　　　 　　　　　　｜");
+                Console.WriteLine ("｜　　　　　　　　您的BMI值正常　　　　　　　 　　　　　　｜") ;
             }
-
-            Console.WriteLine("＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝　　　　　　　");
+            Console.WriteLine ("＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝　　　　　　　") ;
 
             Console.ReadLine();
 
